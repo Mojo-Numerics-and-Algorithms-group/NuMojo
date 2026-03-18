986e37c4463aeaadeedc9f48b2fc99e13fde4328
shivasankar <shivasankar.ka@gmail.com>
2026-03-18 21:37:34 +0900

    delete science module into oblivion

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -3,4 +3,3 @@ set -e  # Exit immediately if any command fails
 
 bash tests/test_core.sh
 bash tests/test_routines.sh
-bash tests/test_science.sh
--- tests/test_science.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e  # Exit immediately if any command fails
-
-for f in tests/science/*.mojo; do
-    pixi run mojo run -I tests/ "$f"
-done
